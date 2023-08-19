c48dd7449a5900a8a1c689aa4daec7acd1938cc3
jannydream <fation.qama.god@gmail.com>
2023-08-19 11:07:12 +0200

    feat: set mock datas

---
+++ src/mockData/index.ts
@@ -0,0 +1,18 @@
+import { tableItemType } from "../types";
+
+export const datas: tableItemType[] = [
+    {
+        id: 1,
+        title: "React 18",
+        published: true,
+        desc: "This is a description of React 18 ",
+        body: "This is React 18",
+    },
+    {
+        id: 2,
+        title: "React 17",
+        published: false,
+        desc: "This is a description of React 17 ",
+        body: "This is React 17",
+    }
+]
